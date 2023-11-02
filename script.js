6917d30fb007875c855d1cecd632950c30f8e8b2
Josepascal <Josepascal@users.noreply.github.com>
2023-11-01 23:08:14 -0400

    Add function that plays a single round of rock paper scissors

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const getComputerChoice = () => {
     let choice = ["Rock", "Paper", "Scissors"]
 
-    return choice[Math.floor(Math.random() * choice.length)]
+    return choice[Math.floor(Math.random() * choice.length)].toLowerCase();
 }
 
-console.log(getComputerChoice())
+// Play a single round of Rock Paper Scissors
+const playRound = (playerSelection, computerSelection) => {
+    let player = playerSelection.toLowerCase();
+    console.log(`Player choice: ${player}`);
+    console.log(`Computer choice: ${computerSelection}`)
+
+   if (player === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (player === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (player === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper";
+    } else if (player === computerSelection) {
+        return "It's a tie!";
+    } else {
+        return "You win!";
+    }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
